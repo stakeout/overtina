2dc833bb3e1000b9ea25cb84de523ee7ebdac4f5
Vadim Novash <novash@tut.by>
2016-12-15 19:31:18 +0300

    reviews styles & work-gallery block

--- app/blocks/stapel/stapelControls.js
+++ app/blocks/stapel/stapelControls.js
@@ -1,17 +1,23 @@
 export default () => {
-	const $grid = $('.tp-grid');
+	const $grid = $('.our-works');
 	const $closeBtn = $('.gallery__close');
 	const $controls = $('.gallery__top-bar');
 	const $title = $('.gallery__name');
+	const $rightColumn = $('.gallery__right-column');
 	const stapel = $grid.stapel({
 		onLoad(){
 			$controls.hide();
 		},
 		onBeforeOpen(pileName) {
+			$controls.show();
 			$title.html(pileName);
+			$rightColumn.hide();
 		},
 		onAfterOpen(){
-			$controls.show();
+		},
+		onBeforeClose(){
+			$rightColumn.show(500);
+
 		}
 	});
 	$closeBtn.on('click', function (){
